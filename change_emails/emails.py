1055ff0af5e911600472c500aa6ee387c6cde67d
AndrewRomanyshyn <morisszalivaet228@mail.ru>
2023-04-05 13:13:17 +0200

    updated task with changing emails

--- change_emails/emails.py
+++ change_emails/emails.py
@@ -1,1 +1,11 @@
+import csv
+
+with open(r'C:\Users\Andrew\csv_tasks\client.csv') as file_read:
+    with open('changed_emails.csv', 'w') as file_write:
+        for line in file_read.read().split():
+            if '.ru' in line:
+                line = line.replace('.ru', '.ua')
+            file_write.write(line + '\n')
+
+
 
